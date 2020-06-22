913069d47fe65808209b4d3e00c10a00daff7de1
frencojobs <frenco.willian@gmail.com>
2020-06-22 15:26:07 +0630

    rewrote api client without retrofit

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,25 +1,79 @@
 import 'package:dio/dio.dart';
-import 'package:retrofit/retrofit.dart';
+import 'package:flutter/foundation.dart';
+import 'package:dio_flutter_transformer/dio_flutter_transformer.dart';
 
 import 'package:scrapbook/models/models.dart';
 
-part 'api.g.dart';
-
 class Api {
-  static final _dio = Dio();
+  static final _dio = (Dio()..transformer = FlutterTransformer());
   static var client = RestClient(_dio);
 }
 
-@RestApi(baseUrl: 'https://scrapbook.hackclub.com/api')
-abstract class RestClient {
-  factory RestClient(Dio dio, {String baseUrl}) = _RestClient;
+class RestClient {
+  final baseUrl = 'https://scrapbook.hackclub.com/api';
+  final Dio dio;
+  RestClient(this.dio);
 
-  @GET('/posts')
-  Future<List<Post>> fetchPosts();
+  static List<Post> _postsFromResponse(List<dynamic> res) {
+    return res
+        .map((dynamic i) => Post.fromJson(i as Map<String, dynamic>))
+        .toList();
+  }
 
-  @GET('/users')
-  Future<List<User>> fetchUsers();
+  static List<User> _usersFromResponse(List<dynamic> res) {
+    return res
+        .map((dynamic i) => User.fromJson(i as Map<String, dynamic>))
+        .toList();
+  }
 
-  @GET('/users/{username}')
-  Future<UserInfo> fetchUserByName(@Path('username') String username);
+  static UserInfo _userInfoFromResponse(Map<String, dynamic> res) {
+    return UserInfo.fromJson(res);
+  }
+
+  Future<List<Post>> fetchPosts() async {
+    final result = await dio.request<List<dynamic>>(
+      '/posts',
+      options: RequestOptions(
+        method: 'GET',
+        headers: <String, dynamic>{},
+        baseUrl: baseUrl,
+      ),
+    );
+
+    final value = await compute(_postsFromResponse, result.data);
+
+    return Future.value(value);
+  }
+
+  Future<List<User>> fetchUsers() async {
+    final result = await dio.request<List<dynamic>>(
+      '/users',
+      options: RequestOptions(
+        method: 'GET',
+        headers: <String, dynamic>{},
+        baseUrl: baseUrl,
+      ),
+    );
+
+    final value = await compute(_usersFromResponse, result.data);
+
+    return Future.value(value);
+  }
+
+  Future<UserInfo> fetchUserByName(String username) async {
+    ArgumentError.checkNotNull(username, 'username');
+
+    final result = await dio.request<Map<String, dynamic>>(
+      '/users/$username',
+      options: RequestOptions(
+        method: 'GET',
+        headers: <String, dynamic>{},
+        baseUrl: baseUrl,
+      ),
+    );
+
+    final value = await compute(_userInfoFromResponse, result.data);
+
+    return Future.value(value);
+  }
 }
